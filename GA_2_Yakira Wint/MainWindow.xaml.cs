48dcdf1fbcfcd10d30887c1cb8e4545be1eebebe
Ywint92 <ynwint@student.rtc.edu>
2023-10-20 06:09:42 -0700

    assigment 2

--- GA_2_Yakira Wint/MainWindow.xaml.cs
+++ GA_2_Yakira Wint/MainWindow.xaml.cs
@@ -29,6 +29,10 @@ namespace GA_2_Yakira_Wint
         }
 
         public object Wint { get; private set; }
+        public object Yakira { get; private set; }
+        public object labelName { get; private set; }
+        public object txtDisplayLetter { get; private set; }
+        public object lblNumberOfLetters { get; private set; }
 
         private void btnFullName_Click(object sender, RoutedEventArgs e)
         {
@@ -43,6 +47,64 @@ namespace GA_2_Yakira_Wint
             MessageBox.Show(fullName);
 
         } // btnFullName_Click
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string number1 = txtNumber1.Text;
+            string number2 = txtNumber2.Text;
+            // Parsing my numbers
+            int num1 = int.Parse(number1);
+            int num2 = int.Parse(number2);
+
+            string sign = "+";
+            string equalSign = "=";
+            // Doing math with our new int
+            int result = num1 + num2;
+
+            string equation = $"{number1} {sign} {number2} {equalSign} {result}";
+
+            // Displaying the equation string
+            lblEquation.Content = equation;
+        }
+
+        private void btnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            string number1 = txtNumber1.Text;
+            string number2 = txtNumber2.Text;
+            int num1 = int.Parse(number1);
+            int num2 = int.Parse(number2);
+
+            string sign = "-"; // Changes + to -
+            string equalSign = "=";
+            int result = num1 - num2; // Changed + to -
+
+            string equation = $"{number1} {sign} {number2} {equalSign} {result}";
+
+            lblEquation.Content = equation;
+        }
+
+        private void txtWord_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string usersWord = txtWord.Text; // Getting users word
+            int stringLength = usersWord.Length; // Getting how many characters are in the word
+            int lastIndex = stringLength - 1; // Getting the last index of the word
+            string formatedString = $"Enter a number between 0 and {lastIndex}"; // Displaying the range of 0 to the last index
+
+            // Displaying it to our label
+            lblNumberOfLetters.Content = formatedString;
+        }
+
+        private void btnDisplayLetter_Click(object sender, RoutedEventArgs e)
+        {
+            string userWord = txtWord.Text;
+            // Grabbing the word and parsing in one line
+            int usersNumber = int.Parse(txtCharIndex.Text);
+            char selectedChar = userWord[usersNumber];
+
+       
+            txtDisplayLetter.Text = selectedChar.ToString(); ;
+            // btnDisplayLetter_Click
+        }
     } 
 
 }
