9975f677b92d797650964383830dc523ff5d8f59
Ywint92 <ynwint@student.rtc.edu>
2023-10-20 03:34:40 -0700

    Assigment 2

--- GA_2_Yakira Wint/MainWindow.xaml.cs
+++ GA_2_Yakira Wint/MainWindow.xaml.cs
@@ -20,9 +20,29 @@ namespace GA_2_Yakira_Wint
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Buttons, Labels, and TextBoxes
+
         public MainWindow()
         {
             InitializeComponent();
+            //MainWindow()
         }
-    }
+
+        public object Wint { get; private set; }
+
+        private void btnFullName_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = txtFirstName.Text;
+            string lastName = txtLastName.Text;
+
+            // Declare a third string, full name
+            // Concatenate ( combine ) the first and last name into one long string
+            string fullName = $"{Yakira} {Wint}";
+
+            // Display full name on button click
+            MessageBox.Show(fullName);
+
+        } // btnFullName_Click
+    } 
+
 }
